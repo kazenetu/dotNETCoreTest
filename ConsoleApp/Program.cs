2f18b618878985dcb7db2fb6f47ddb7aea5379df
kazenetu <rakudafactory@gmail.com>
2017-11-04 00:10:38 +0900

    SQL発行サンプルを追加

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace ConsoleApp
 {
@@ -10,6 +12,33 @@ namespace ConsoleApp
 
             var hellow = new Hellow("C#");
             Console.WriteLine(hellow.GetFormatName());
+
+            // SQL発行
+            Console.WriteLine(selectSql());
+        }
+
+        private static string selectSql()
+        {
+            var resourcePath = AppContext.BaseDirectory;
+            resourcePath = Path.Combine(resourcePath,"Resource/Test.db");
+
+            // SQL実行
+            using(var db = new SQLiteDB("Data Source=" + resourcePath))
+            {
+                var sql = new StringBuilder();
+                sql.AppendLine("select");
+                sql.AppendLine("  USER_NAME");
+                sql.AppendLine("from");
+                sql.AppendLine("  MT_USER");
+
+                var result = db.Fill(sql.ToString());
+                if (result.Rows.Count <= 0)
+                {
+                    return string.Empty;
+                }
+
+                return result.Rows[0]["USER_NAME"].ToString();
+            }
         }
     }
 }
