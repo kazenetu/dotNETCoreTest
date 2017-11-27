2bb8f11e813049a70a2ca41b8d6c383f2cd26839
kazenetu <rakudafactory@gmail.com>
2017-11-27 22:45:15 +0900

    DBアクセスサンプル追加

---
+++ WebApp/WebApiSample/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Commons.Interfaces;
+using Commons.DB;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace WebApiSample.Controllers
+{
+  [Route("api/[controller]")]
+  public class UsersController : Controller
+  {
+    // POST api/Users
+    [HttpPost]
+    public JsonResult Post([FromBody]Dictionary<string, object> param)
+    {
+      var data = new Dictionary<string, object>();
+
+      // TODO DatabaseFactoryクラス作成
+      var resourcePath = AppContext.BaseDirectory;
+      resourcePath = Path.Combine(resourcePath, "Resource/Test.db");
+
+      // SQL実行
+      var userName = string.Empty;
+      using (var db = new SQLiteDB("Data Source=" + resourcePath) as IDatabase)
+      {
+        userName = Models.Users.getUserName(db, param["userID"].ToString());
+      }
+      data.Add(nameof(userName), userName);
+
+      var result = new Dictionary<string, Dictionary<string, object>>();
+      result.Add("value", data);
+
+      return Json(result);
+    }
+
+  }
+}
---
+++ WebApp/WebApiSample/Models/Users.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using Commons.Interfaces;
+
+namespace Models
+{
+  public class Users
+  {
+    #region パブリックメソッド
+
+    /// <summary>
+    /// ユーザー名取得用Select発行
+    /// </summary>
+    /// <param name="db">databaseインスタンス</param>
+    /// <param name="userId">ユーザーID</param>
+    /// <returns>ユーザー名</returns>
+    public static string getUserName(IDatabase db, string userId)
+    {
+      var sql = new StringBuilder();
+      sql.AppendLine("select");
+      sql.AppendLine("  USER_NAME");
+      sql.AppendLine("from");
+      sql.AppendLine("  MT_USER");
+      sql.AppendLine("where ");
+      sql.AppendLine("  USER_ID = @USER_ID");
+
+      // Param設定
+      db.ClearParam();
+      db.AddParam("@USER_ID", userId);
+
+      var result = db.Fill(sql.ToString());
+      if (result.Rows.Count <= 0)
+      {
+        return string.Empty;
+      }
+
+      return result.Rows[0]["USER_NAME"].ToString();
+    }
+
+    #endregion
+  }
+}
