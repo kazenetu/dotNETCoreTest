27ea9bb84e77cb7f8d0f79e4495ed4ae3500bb31
kazenetu <rakudafactory@gmail.com>
2017-11-28 21:54:52 +0900

    DBインスタンス生成クラスを追加

---
+++ WebApp/WebApiSample/Commons/DatabaseFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Commons.Interfaces;
+
+namespace Commons.DB
+{
+  /// <summary>
+  /// DBインスタンス生成クラス
+  /// </summary>
+  public class DatabaseFactory
+  {
+    /// <summary>
+    /// DBインスタンス取得
+    /// </summary>
+    /// <returns>IDatabasインスタンス</returns>
+    public static IDatabase Create()
+    {
+
+      // TODO 設定ファイルによるインスタンス生成
+      var resourcePath = AppContext.BaseDirectory;
+      resourcePath = Path.Combine(resourcePath, "Resource/Test.db");
+
+      return new SQLiteDB("Data Source=" + resourcePath);
+    }
+  }
+}
