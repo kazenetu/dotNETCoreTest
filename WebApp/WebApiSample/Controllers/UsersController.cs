a700c735358fbff669dc52eca0a82508723d8de4
kazenetu <rakudafactory@gmail.com>
2017-11-30 22:50:52 +0900

    DBインスタンス取得からDIで取得するように修正

--- WebApp/WebApiSample/Controllers/UsersController.cs
+++ WebApp/WebApiSample/Controllers/UsersController.cs
@@ -7,12 +7,19 @@ using Commons.Interfaces;
 using Commons.DB;
 using Microsoft.AspNetCore.Mvc;
 using Models;
+using Interfaces;
 
 namespace WebApiSample.Controllers
 {
   [Route("api/[controller]")]
   public class UsersController : Controller
   {
+    private readonly IUserRepository repository;
+
+    public UsersController(IUserRepository repository){
+      this.repository = repository;
+    }
+
     // POST api/Users
     [HttpPost]
     public JsonResult Post([FromBody]Dictionary<string, object> param)
@@ -20,11 +27,7 @@ namespace WebApiSample.Controllers
       var data = new Dictionary<string, object>();
 
       // SQL実行
-      var userName = string.Empty;
-      using (var db = DatabaseFactory.Create())
-      {
-        userName = Models.Users.getUserName(db, param["userID"].ToString());
-      }
+      var userName = repository.getUserName(param["userID"].ToString());
       data.Add(nameof(userName), userName);
 
       var result = new Dictionary<string, Dictionary<string, object>>();
