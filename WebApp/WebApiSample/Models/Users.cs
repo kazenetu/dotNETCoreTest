68de7a63c32d170513aa71f3d83339bc7916569f
kazenetu <rakudafactory@gmail.com>
2017-12-01 23:11:45 +0900

    ユーザークラスの仕様を変更

--- WebApp/WebApiSample/Models/Users.cs
+++ WebApp/WebApiSample/Models/Users.cs
@@ -1,5 +1,4 @@
-﻿using System.Text;
-using Commons.Interfaces;
+﻿using Interfaces;
 
 namespace Models
 {
@@ -7,33 +6,9 @@ namespace Models
   {
     #region パブリックメソッド
 
-    /// <summary>
-    /// ユーザー名取得用Select発行
-    /// </summary>
-    /// <param name="db">databaseインスタンス</param>
-    /// <param name="userId">ユーザーID</param>
-    /// <returns>ユーザー名</returns>
-    public static string getUserName(IDatabase db, string userId)
+    public static string getUserName(IUserRepository repository, string userId)
     {
-      var sql = new StringBuilder();
-      sql.AppendLine("select");
-      sql.AppendLine("  USER_NAME");
-      sql.AppendLine("from");
-      sql.AppendLine("  MT_USER");
-      sql.AppendLine("where ");
-      sql.AppendLine("  USER_ID = @USER_ID");
-
-      // Param設定
-      db.ClearParam();
-      db.AddParam("@USER_ID", userId);
-
-      var result = db.Fill(sql.ToString());
-      if (result.Rows.Count <= 0)
-      {
-        return string.Empty;
-      }
-
-      return result.Rows[0]["USER_NAME"].ToString();
+      return string.Format("Hello! {0}", repository.getUserName(userId));
     }
 
     #endregion
