76f20866a200577064ac620c143efe100f9e4e08
kazenetu <rakudafactory@gmail.com>
2017-12-02 22:27:23 +0900

    サービスクラスのDI設定を追加

--- WebApp/WebApiSample/Startup.cs
+++ WebApp/WebApiSample/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using service;
 
 namespace WebApiSample
 {
@@ -29,6 +30,7 @@ namespace WebApiSample
 
             // DIのテスト
             services.AddScoped<IUserRepository,UserRepository>();
+            services.AddScoped<IUserService ,UserService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
