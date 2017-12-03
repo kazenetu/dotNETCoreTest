b666ea17d582bb71faf374ab82fc1c60113752df
kazenetu <rakudafactory@gmail.com>
2017-12-03 22:12:31 +0900

    Modelクラスのスーパークラスを作成

---
+++ WebApp/WebApiSample/Models/ModelBase.cs
@@ -0,0 +1,17 @@
+namespace Models
+{
+  /// <summary>
+  /// Modelクラスのスーパークラス
+  /// </summary>
+  public abstract class ModelBase<T>  where T :  new()
+  {
+
+    /// <summary>
+    /// Modelクラスのインスタンス作成
+    /// </summary>
+    public static T Create()
+    {
+      return new T();
+    }
+  }
+}
