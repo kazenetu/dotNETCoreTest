7361d645201e1e988615d698cc414bbf78d339c2
kazenetu <rakudafactory@gmail.com>
2017-12-03 22:12:52 +0900

    Modelスーパークラスを継承するように修正

--- WebApp/WebApiSample/Models/User.cs
+++ WebApp/WebApiSample/Models/User.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Models
 {
-  public class User
+  public class User : ModelBase<User>
   {
     public string UserId { set; get; }
     public string UserName { set; get; }
@@ -10,10 +10,5 @@ namespace Models
     public DateTime DataDate { set; get; }
     public DateTime TimeDate { set; get; }
     public DateTime TsDate { set; get; }
-
-    public static User Create()
-    {
-      return new User();
-    }
   }
 }
