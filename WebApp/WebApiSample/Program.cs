91d0133c0a7057802bdbf620f312682a34fdf81c
kazenetu <rakudafactory@gmail.com>
2017-12-11 22:27:53 +0900

    ログ出力の対象外設定

--- WebApp/WebApiSample/Program.cs
+++ WebApp/WebApiSample/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Debug;
 
 namespace WebApiSample
 {
@@ -19,7 +20,10 @@ namespace WebApiSample
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
+            .ConfigureLogging(logging =>
+                logging.AddFilter("System", LogLevel.None)
+                        .AddFilter<DebugLoggerProvider>("Microsoft", LogLevel.None))
+                        .UseStartup<Startup>()
                 .Build();
     }
 }
