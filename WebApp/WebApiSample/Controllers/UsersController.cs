0cfe5a2e0d8cb3569e455721a1df39d459586546
kazenetu <rakudafactory@gmail.com>
2017-12-11 22:36:04 +0900

    ログ出力を追加

--- WebApp/WebApiSample/Controllers/UsersController.cs
+++ WebApp/WebApiSample/Controllers/UsersController.cs
@@ -8,6 +8,8 @@ using Commons.DB;
 using Microsoft.AspNetCore.Mvc;
 using Models;
 using Interfaces;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace WebApiSample.Controllers
 {
@@ -15,9 +17,11 @@ namespace WebApiSample.Controllers
   public class UsersController : Controller
   {
     private readonly IUserService service;
-
-    public UsersController(IUserService service){
+    private readonly ILogger logger;
+    
+    public UsersController(IUserService service,ILogger<UsersController> logger){
       this.service = service;
+      this.logger = logger;
     }
 
     // POST api/Users
@@ -30,12 +34,14 @@ namespace WebApiSample.Controllers
 
       // 入力チェック
       if(!param.ContainsKey(paramNameUserId)){
+        logger.LogError("Pram[{0}]が未設定",paramNameUserId);
         return BadRequest();
       }
 
       // パラメータ取得
       var user = Models.User.Create();
       user.UserId = param["userID"].ToString();
+      logger.LogDebug("Pram[{0}] = {1}",paramNameUserId,user.UserId);
 
       // サービスのメソッドを呼び出し
       var userName = service.getUserName(user);
@@ -43,6 +49,8 @@ namespace WebApiSample.Controllers
 
       var result = new Dictionary<string, Dictionary<string, object>>();
       result.Add("value", data);
+
+      logger.LogDebug("result = {0}",JsonConvert.SerializeObject(result));
 
       return Json(result);
     }
