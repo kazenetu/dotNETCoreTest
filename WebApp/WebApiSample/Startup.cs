d07e47959c8d8d3e1017808a9d301c47f598eadc
kazenetu <rakudafactory@gmail.com>
2017-12-20 22:28:52 +0900

    ルートアクセス時にトークンを発行するように機能追加

--- WebApp/WebApiSample/Startup.cs
+++ WebApp/WebApiSample/Startup.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Commons.ConfigModel;
 using Infrastructure;
 using Interfaces;
+using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -29,6 +31,8 @@ namespace WebApiSample
     {
       services.AddMvc();
 
+      services.AddAntiforgery(options => options.HeaderName = "X-XSRF-TOKEN");
+
       // DIのテスト
       services.AddScoped<IUserRepository, UserRepository>();
       services.AddScoped<IUserService, UserService>();
@@ -38,13 +42,29 @@ namespace WebApiSample
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, IAntiforgery antiforgery)
     {
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
       }
       app.UseMvc();
+
+      // ルートアクセス時にトークン発行
+      app.Use(next => context =>
+      {
+        if (
+          string.Equals(context.Request.Path.Value, "/", StringComparison.OrdinalIgnoreCase) ||
+          string.Equals(context.Request.Path.Value, "/index.html", StringComparison.OrdinalIgnoreCase))
+        {
+          // We can send the request token as a JavaScript-readable cookie, and Angular will use it by default.
+          var tokens = antiforgery.GetAndStoreTokens(context);
+          context.Response.Cookies.Append("XSRF-TOKEN", tokens.RequestToken,
+            new CookieOptions() { HttpOnly = false });
+        }
+
+        return next(context);
+      });
 
       // 静的ファイルのデフォルト設定を有効にする
       app.UseDefaultFiles();
