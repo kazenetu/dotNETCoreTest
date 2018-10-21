658ba6b19efebd0ffe5b3d78b7c229ffdce16407
kazenetu <rakudafactory@gmail.com>
2018-10-21 18:16:45 +0900

    定数クラス追加

---
+++ WebApp21/WebApiSample21/Statics.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAPI
+{
+  /// <summary>
+  /// 定数クラス
+  /// </summary>
+  public static class Statics
+  {
+    /// <summary>
+    /// セッション用クッキー名
+    /// </summary>
+    public static readonly string SessionCookieName = "sid";
+  }
+}
