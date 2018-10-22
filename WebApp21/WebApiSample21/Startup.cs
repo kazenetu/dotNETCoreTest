cb89b1112dca71d052451320f1c43640d0a4c949
kazenetu <rakudafactory@gmail.com>
2018-10-23 06:48:41 +0900

    トークン発行、セッション設定を追加

--- WebApp21/WebApiSample21/Startup.cs
+++ WebApp21/WebApiSample21/Startup.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using WebAPI;
 
 namespace WebApiSample21
 {
@@ -26,22 +29,60 @@ namespace WebApiSample21
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // トークン発行
+            services.AddAntiforgery(options => options.HeaderName = "X-XSRF-TOKEN");
+
+            // session
+            services.AddSession(options =>
+            {
+              // Set a short timeout for easy testing.
+              // options.IdleTimeout = TimeSpan.FromSeconds(10);
+              // options.Cookie.HttpOnly = true;
+              options.Cookie.Name = Statics.SessionCookieName;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IAntiforgery antiforgery)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                //app.UseDeveloperExceptionPage();
             }
             else
             {
-                app.UseHsts();
+                //app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
+
+            // session設定
+            app.UseSession();
+
             app.UseMvc();
-        }
+
+            // ルートアクセス時にトークン発行
+            app.Use(next => context =>
+            {
+              if (
+                string.Equals(context.Request.Path.Value, "/", StringComparison.OrdinalIgnoreCase) ||
+                string.Equals(context.Request.Path.Value, "/index.html", StringComparison.OrdinalIgnoreCase))
+              {
+                // We can send the request token as a JavaScript-readable cookie, and Angular will use it by default.
+                var tokens = antiforgery.GetAndStoreTokens(context);
+                context.Response.Cookies.Append("XSRF-TOKEN", tokens.RequestToken,
+                  new CookieOptions() { HttpOnly = false });
+              }
+
+              return next(context);
+            });
+
+            // 静的ファイルのデフォルト設定を有効にする
+            app.UseDefaultFiles();
+
+            // 静的ファイルを使用する
+            app.UseStaticFiles();
+          }
     }
 }
