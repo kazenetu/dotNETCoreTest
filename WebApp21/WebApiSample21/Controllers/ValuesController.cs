5d86ca74abd865e0656cbd5c0675f0b1f9e9ccc2
kazenetu <rakudafactory@gmail.com>
2018-10-27 17:12:27 +0900

    不要なusingを削除

--- WebApp21/WebApiSample21/Controllers/ValuesController.cs
+++ WebApp21/WebApiSample21/Controllers/ValuesController.cs
@@ -1,12 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace WebApiSample21.Controllers
 {
-    [Route("api/[controller]")]
+  [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
